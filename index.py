6432234d8b050cc7d5674ac158da511ca101a08e
awesome-apps <scideastech6@gmail.com>
2014-10-01 21:42:32 -0700

    Update index.py

--- index.py
+++ index.py
@@ -6,19 +6,24 @@ def show():
     print '|',board[3],'|',board[4],'|',board[5],'|'
     print "_____________"
     print '|',board[6],'|',board[7],'|',board[8],'|'
+    print "_____________"
 show()
+print "The Tic Tac Toe Board looks like the one above. Each square has a number which is a position. The program keeps providing inputs for the user to select that position. You both have a total of nine turns to beat your opponent. X starts the game. Good Luck!"
 turn = 1
 turns = 0
 while turns < 9:
     pos = int(input("Enter a position"))
+    if pos >= 9:
+        print "You were supposed to enter out a number between zero and eight"
     realTurn = turn%2
     if realTurn == 0:
         board[pos] = 'o'
     else:
         board[pos] = 'x'
+    print "You have %i turns left" % turn
     show()
-    turn += 1
     turns += 1
+    turn += 1
     if board[0] == 'x' and board[1] == 'x' and board[2] == 'x':
         print "X WINS"
         break
@@ -67,4 +72,3 @@ while turns < 9:
     elif board[2] == 'o' and board[4] == 'o' and board[6] == 'o':
         print "O WINS"
         break
-print "Game OVER"
